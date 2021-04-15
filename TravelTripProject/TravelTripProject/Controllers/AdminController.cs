60e88b51ff648afcd73c9838a4022bb9a9539dde
Furkan <63557241+furkanyasar19@users.noreply.github.com>
2021-04-16 02:18:26 +0300

    TravelTrip_MVC
    TravelTrip_MVC Project

---
+++ TravelTripProject/TravelTripProject/Controllers/AdminController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        [Authorize]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Authorize]
+        public ActionResult YeniBlog(Blog p)
+        {
+            //Yeni Blog Ekleme
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id)
+        {
+            //Blog Silme
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public ActionResult BlogGetir(int id)
+        {
+            //Güncelle sayfasına bilgileri getiriyor
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.Aciklama = b.Aciklama;
+            blg.Baslik = b.Baslik;
+            blg.BlogImage = b.BlogImage;
+            blg.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [Authorize]
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            //Yorum Silme
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        [Authorize]
+        public ActionResult YorumGetir(int id)
+        {
+            //Yorum sayfasına bilgileri getiriyor
+            var yr = c.Yorumlars.Find(id);
+            return View("YorumGetir", yr);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.ID);
+            yrm.KullaniciAdi = y.KullaniciAdi;
+            yrm.Mail = y.Mail;
+            yrm.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+    }
+}
---
+++ TravelTripProject/TravelTripProject/Controllers/DefaultController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.Take(4).ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            //Ana sayfadaki ilk üç bloğun ilk ikisini getiriyor. Tasarımdan Dolayı böyle parça parça getirmek zorunda kaldık
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            //Ana sayfadaki ilk üç bloğun sondakini getiriyor. Tasarımdan Dolayı böyle parça parça getirmek zorunda kaldık
+            var deger = c.Blogs.Where(x => x.ID == 5).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            //TOP 10 Kategorileri getiriyor
+            var deger = c.Blogs.Take(10).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            //En Beğenilen Yerleri Getiriyor (İlk 3 tane)
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            //En Beğenilen Yerleri Getiriyor (İlk 3 tane)
+            var deger = c.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList();
+            return PartialView(deger);
+        }
+    }
+}
